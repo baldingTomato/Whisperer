5d169e6ca41966aae845f3c4c5b4c208fdfee82d
baldingTomato <tomek.kluba12@gmail.com>
2024-09-17 20:40:00 +0200

    Added virtual keyword to the interface

--- src/file_input_device.hpp
+++ src/file_input_device.hpp
@@ -1,8 +1,10 @@
+#pragma once
+
 #include "input_device.hpp"
 
 class FileInputDevice : public InputDevice {
 public:
-    int openDevice(const char* device, int flags) override;
-    ssize_t readEvent(int fd, void* buffer, size_t count) override;
-    int closeDevice(int fd) override;
+    virtual int openDevice(const char* device, int flags) override;
+    virtual ssize_t readEvent(int fd, void* buffer, size_t count) override;
+    virtual int closeDevice(int fd) override;
 };
