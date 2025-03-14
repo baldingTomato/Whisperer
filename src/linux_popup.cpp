ea3c160f2e5c23b45329941a2010f43fd63e6f40
baldingTomato <tomek.kluba12@gmail.com>
2025-03-14 18:14:37 +0100

    Adjusted linux popup to warp longer text

--- src/linux_popup.cpp
+++ src/linux_popup.cpp
@@ -4,6 +4,7 @@
 #include <QGraphicsDropShadowEffect>
 #include <QLabel>
 #include <QScreen>
+#include <QRegularExpression>
 #include <QTimer>
 #include <QWidget>
 
@@ -13,6 +14,12 @@ void LinuxPopup::popWindow(const QString& translatedText) {
 
     QLabel* label = new QLabel(translatedText, popup);
     label->setAlignment(Qt::AlignCenter);
+    label->setStyleSheet(
+        "QLabel {"
+        "    font-size: 12pt;"
+        "}");
+
+    label->setWordWrap(true);
 
     int marginSize = 10;
     label->setContentsMargins(marginSize, marginSize, marginSize, marginSize);
@@ -21,7 +28,19 @@ void LinuxPopup::popWindow(const QString& translatedText) {
     label->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
     label->adjustSize();
 
+    // ---- DYNAMIC DISPLAY DURATION ----
+    // 3 seconds base + 300ms per word, capped at 10 seconds total
+    int baseDurationMs = 3000;
+    QStringList words = translatedText.split(QRegularExpression("\\s+"), Qt::SkipEmptyParts);
+    int wordCount = words.size();
+    int extraMsPerWord = 300;
+    int displayDurationMs = baseDurationMs + (wordCount * extraMsPerWord);
+
+    if (displayDurationMs > 10000) {
+        displayDurationMs = 10000;
+    }
+
     popup->show();
 
-    QTimer::singleShot(3000, popup, &QWidget::close);
+    QTimer::singleShot(displayDurationMs, popup, &QWidget::close);
 }
