c7a1a0f1dd407c8e3b921449bc67dc427dbb8087
baldingTomato <tomek.kluba12@gmail.com>
2025-08-25 17:48:30 +0200

    Small code structure change for clarity

--- src/core/lingva_api.cpp
+++ src/core/lingva_api.cpp
@@ -1,12 +1,15 @@
 #include "lingva_api.hpp"
 
+#include <cpr/cpr.h>
+#include <stdexcept>
+
 std::string LingvaAPI::requestTranslation(const std::string& text) const {
     std::string url = "https://lingva.ml/api/v1/en/pl/" + cpr::util::urlEncode(text);
-    cpr::Response r = cpr::Get(cpr::Url{url});
+    const cpr::Response r = cpr::Get(cpr::Url{url});
 
-    if (r.status_code == 200) {
+    if (r.status_code == HTTP_OK) {
         return r.text;
-    } else {
-        throw std::runtime_error("Lingva API request failed with status code " + std::to_string(r.status_code));
     }
+
+    throw std::runtime_error("Lingva API request failed with status code " + std::to_string(r.status_code));
 }
--- src/core/lingva_api.hpp
+++ src/core/lingva_api.hpp
@@ -1,7 +1,10 @@
-#include <cpr/cpr.h>
+#pragma once
+
 #include "translation_api.hpp"
 
 class LingvaAPI : public TranslationAPI {
+    static constexpr int HTTP_OK = 200;
+
 public:
     virtual std::string requestTranslation(const std::string& text) const override;
 };
