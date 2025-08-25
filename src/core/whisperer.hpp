0f04ecc4cfa89f08fad94815d5c9184ce78e7043
baldingTomato <tomek.kluba12@gmail.com>
2025-08-25 19:34:12 +0200

    Code formatting

--- src/core/whisperer.hpp
+++ src/core/whisperer.hpp
@@ -12,13 +12,13 @@ class Whisperer {
     std::unique_ptr<Listener> listener_;
     std::unique_ptr<Translator> translator_;
     std::unique_ptr<Popup> popup_;
-    std::atomic<bool> keepListening_{ true };
+    std::atomic<bool> keepListening_{true};
 
     void displayTranslationPopup(const std::string& translatedText) const;
 
 public:
     Whisperer(std::unique_ptr<Listener> listener, std::unique_ptr<Translator> translator, std::unique_ptr<Popup> popup)
-        : listener_(std::move(listener)), translator_(std::move(translator)), popup_(std::move(popup)){};
+        : listener_(std::move(listener)), translator_(std::move(translator)), popup_(std::move(popup)) {};
 
     void startListening();
 };
