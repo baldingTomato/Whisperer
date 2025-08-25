bf9e5e7a0b2423838277b89f97285329d21b4541
baldingTomato <tomek.kluba12@gmail.com>
2025-08-25 18:11:54 +0200

    Changed access modifier for displaying a popup

--- src/core/whisperer.hpp
+++ src/core/whisperer.hpp
@@ -6,6 +6,7 @@
 
 #include <atomic>
 #include <memory>
+#include <utility>
 
 class Whisperer {
     std::unique_ptr<Listener> listener_;
@@ -13,10 +14,11 @@ class Whisperer {
     std::unique_ptr<Popup> popup_;
     std::atomic<bool> keepListening_{ true };
 
+    void displayTranslationPopup(const std::string& translatedText) const;
+
 public:
     Whisperer(std::unique_ptr<Listener> listener, std::unique_ptr<Translator> translator, std::unique_ptr<Popup> popup)
         : listener_(std::move(listener)), translator_(std::move(translator)), popup_(std::move(popup)){};
 
     void startListening();
-    void displayTranslationPopup(const std::string& translatedText) const;
 };
