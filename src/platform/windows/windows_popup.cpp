55634b836d62e05d8df51a8e0b85bbf94413a07d
baldingTomato <tomek.kluba12@gmail.com>
2025-08-28 17:56:57 +0200

    Refactoring of popup window for better code readability

--- src/platform/windows/windows_popup.cpp
+++ src/platform/windows/windows_popup.cpp
@@ -14,53 +14,69 @@
 #include <QVBoxLayout>
 #include <QWidget>
 
-void WindowsPopup::popWindow(const QString& translatedText) const {
-    QWidget* popup = new QWidget();
-    popup->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Tool);
-    popup->setAttribute(Qt::WA_TranslucentBackground);
-
-    QWidget* container = new QWidget(popup);
-    container->setObjectName("popupContainer");
-    
-    QLabel* label = new QLabel(translatedText, container);
+QLabel* WindowsPopup::createLabel(const QString& text, QWidget* parent) const {
+    QLabel* label = new QLabel(text, parent);
     label->setAlignment(Qt::AlignCenter);
     label->setWordWrap(true);
-    label->setStyleSheet(
+    label->setStyleSheet(QString(
         "QLabel {"
         "    color: white;"
-        "    font-size: 13pt;"
+        "    font-size: %1pt;"
         "    font-family: 'Montserrat', 'Tahoma', 'Arial';"
         "    padding: 8px 16px;"
-        "}");
+        "}").arg(FONT_SIZE_PT));
+    return label;
+}
+
+QWidget* WindowsPopup::createContainer(QLabel* label, QWidget* parent) const {
+    QWidget* container = new QWidget(parent);
+    container->setObjectName("popupContainer");
 
     QVBoxLayout* layout = new QVBoxLayout(container);
     layout->addWidget(label);
     layout->setContentsMargins(16, 12, 16, 12);
 
-    container->setStyleSheet(
+    container->setStyleSheet(QString(
         "#popupContainer {"
-        "    background-color: rgb(40, 20, 50);"  // solid dark background
-        "    border: 2px solid rgba(255, 182, 193, 180);"  // light pink border
-        "    border-radius: 17px;"
-        "}");
+        "    background-color: rgb(40, 20, 50);"
+        "    border: 2px solid rgba(255, 182, 193, 180);"
+        "    border-radius: %1px;"
+        "}").arg(BORDER_RADIUS_PX));
 
-    QGraphicsDropShadowEffect* shadowEffect = new QGraphicsDropShadowEffect(popup);
-    shadowEffect->setBlurRadius(20);
-    shadowEffect->setOffset(0, 6);
+    return container;
+}
+
+void WindowsPopup::applyDropShadow(QWidget* container, QWidget* parent) const {
+    QGraphicsDropShadowEffect* shadowEffect = new QGraphicsDropShadowEffect(parent);
+    shadowEffect->setBlurRadius(SHADOW_BLUR_RADIUS);
+    shadowEffect->setOffset(0, SHADOW_OFFSET_Y);
     shadowEffect->setColor(QColor(0, 0, 0, 100));
     container->setGraphicsEffect(shadowEffect);
+}
 
-    container->adjustSize();
-    popup->resize(container->sizeHint());
-
+void WindowsPopup::centerPopup(QWidget* popup) const {
     QScreen* screen = QGuiApplication::primaryScreen();
     QRect screenGeometry = screen->geometry();
     int x = (screenGeometry.width() - popup->width()) / 2;
     int y = (screenGeometry.height() - popup->height()) / 2;
     popup->move(x, y);
+}
 
+void WindowsPopup::popWindow(const QString& translatedText) const {
+    QWidget* popup = new QWidget();
+    popup->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Tool);
+    popup->setAttribute(Qt::WA_TranslucentBackground);
+
+    QLabel* label = createLabel(translatedText, popup);
+    QWidget* container = createContainer(label, popup);
+    applyDropShadow(container, popup);
+
+    container->adjustSize();
+    popup->resize(container->sizeHint());
+
+    centerPopup(popup);
     popup->show();
 
-    QTimer::singleShot(3000, popup, &QWidget::close);
+    QTimer::singleShot(POPUP_DURATION_MS, popup, &QWidget::close);
 }
 
--- src/platform/windows/windows_popup.hpp
+++ src/platform/windows/windows_popup.hpp
@@ -1,13 +1,29 @@
 #pragma once
 #include "../../core/popup.hpp"
 
+class QLabel;
+class QWidget;
+class QString;
+
 class WindowsPopup : public QObject, public Popup {
     Q_OBJECT
 public:
     explicit WindowsPopup(QObject* parent = nullptr)
         : QObject(parent) {}
 
+    static constexpr int FONT_SIZE_PT = 13;
+    static constexpr int BORDER_RADIUS_PX = 17;
+    static constexpr int SHADOW_BLUR_RADIUS = 20;
+    static constexpr int SHADOW_OFFSET_Y = 6;
+    static constexpr int POPUP_DURATION_MS = 3000;
+
 public slots:
     void popWindow(const QString& translatedText) const override;
     QObject* toQObject() override { return this; }
+
+private:
+    QLabel* createLabel(const QString& text, QWidget* parent) const;
+    QWidget* createContainer(QLabel* label, QWidget* parent) const;
+    void applyDropShadow(QWidget* container, QWidget* parent) const;
+    void centerPopup(QWidget* popup) const;
 };
